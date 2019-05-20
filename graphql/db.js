ec79f35bb8029ea9258bfc78837bdd31f7a893e6
Kim <a01088570194@gmail.com>
2019-05-20 15:50:09 +0900

    Feat: 외부 서버 json파일 데이터 가져오기

--- graphql/db.js
+++ graphql/db.js
@@ -1,30 +1,8 @@
-let movies = [
-  { id: 0, name: "Star Wars", score: 18 },
-  { id: 1, name: "The Godfather I", score: 28 },
-  { id: 2, name: "Lee", score: 13 },
-  { id: 3, name: "Park", score: 58 },
-  { id: 4, name: "John", score: 48 },
-  { id: 5, name: "Sam", score: 17 }
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getById = id => movies.filter(movie => id === movie.id)[0];
-
-export const deleteMovie = id => {
-  const filterdMovies = movies.filter(movie => movie.id !== id);
-  if (filterdMovies.length !== movies.length) {
-    movies = filterdMovies;
-    return true;
-  } else return false;
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies = [...movies, newMovie];
-  return newMovie;
+export const getMovies = (limit, rating) => {
+  return fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 };
